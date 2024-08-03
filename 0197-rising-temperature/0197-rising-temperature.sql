88e285de30703ce37f549b1ce5cc914cd1203bbf
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-08-03 20:06:02 +0900

    Time: 302 ms (98.17%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,10 +1,15 @@
 # Write your MySQL query statement below
 # 전날보다 기온이 높은 날의 id 찾기
 
-SELECT id
-FROM Weather w1
-WHERE temperature > (
-    SELECT temperature
-    FROM Weather w2
-    WHERE w2.recordDate = DATE_SUB(w1.recordDate, INTERVAL 1 DAY)
+WITH q1 AS (
+    SELECT
+        *,
+        LAG(temperature) OVER (ORDER BY recordDate) AS previous_day_temperature,
+        LAG(recordDate) OVER (ORDER BY recordDate) AS previous_Date
+    FROM Weather
 )
+
+SELECT id
+FROM q1
+WHERE temperature > previous_day_temperature
+AND DATEDIFF(recordDate, previous_Date) = 1
