913ed3fa3a527929605f0bc6d7f7ebccdb0467ee
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-08-07 23:05:33 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,9 +1,9 @@
 # 학생 별 시험에 참석한 횟수 세기 / 참석한 적 없다면 0번
 # std_id, sub name 순 정렬
-SELECT std.student_id, std.student_name, sub.subject_name, COUNT(ex.student_id) AS attended_exams
-FROM Students std
-    CROSS JOIN  Subjects sub
-    LEFT JOIN Examinations ex
-    ON std.student_id = ex.student_id AND sub.subject_name = ex.subject_name
-GROUP BY std.student_id, std.student_name, sub.subject_name
-ORDER BY std.student_id, sub.subject_name  
+SELECT *
+FROM Students st
+    CROSS JOIN Subjects su
+    LEFT JOIN Examinations e
+        ON st.student_id = e.student_id AND su.subject_name = e.subject_name
+
+
