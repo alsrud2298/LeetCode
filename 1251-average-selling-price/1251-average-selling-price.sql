8be65420aa16dd49cb691da1421e2d7ec6ab789b
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-08-08 19:53:53 +0900

    Time: 767 ms (34.51%), Space: 0B (100.00%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -2,10 +2,11 @@
 # average selling price : sum(units * price) / total units
 
 -- 1. 조인 조건 : product_id, purchase_date가 start,end date 내에 포함될 것
+-- 2. Price 테이블에 있는 product가 판매되지 않은 경우도 고려해야함.
 
 SELECT p.product_id, IFNULL(ROUND(SUM(p.price*s.units)/SUM(s.units),2),0) as average_price
 FROM Prices p 
     LEFT JOIN UnitsSold s
         ON p.product_id = s.product_id 
-        AND s.purchase_date >= p.start_date AND s.purchase_date <= p.end_date
+        AND s.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY product_id
