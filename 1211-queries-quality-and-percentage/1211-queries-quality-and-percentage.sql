5f53b3de2bdd8654dcc3fec875d5dc8bf913ae81
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-08-09 15:16:52 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,9 @@
+# poor query : rating < 3 
+# query quality : AVG(rating / position)
+# 쿼리 이름, quality, poor_query_percentage 구하기
+
+SELECT query_name
+    , ROUND(AVG(rating/position),2) as quality
+    , ROUND((SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)*100 / COUNT(*)),2) as poor_query_percentage
+FROM Queries
+GROUP BY query_name
