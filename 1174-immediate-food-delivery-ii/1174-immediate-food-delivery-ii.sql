bf9529166a6e1fe3c2a2a320361ed7734c38db65
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-08-12 21:53:45 +0900

    Time: 554 ms (73.38%), Space: 0B (100.00%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,5 +1,9 @@
 # 주문 날짜 = 고객 선호 날짜 이면, immediate. 아니라면, scheduled
 # 각 고객들 첫번째 주문에 대한 immediate orders 비율 구하기
-SELECT delivery_id, DISTINCT(order_id)
-FROM Delevery
-WHERE order_date = customer_pref_delivery_date
+SELECT ROUND(AVG(order_date=customer_pref_delivery_date)*100,2) as immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN(
+    SELECT customer_id, min(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+)
