ea1d7bf32b49a56daa41d19ec0f3198515dc188d
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-08-17 16:52:13 +0900

    Time: 505 ms (65.23%), Space: 0B (100.00%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,19 +1,16 @@
 # 한 부서에만 속한 경우 - primary = 'N'
 # 모든 employee들의 primary department 추출하기
 
-# 1. employee 별 속한 부서 수 구하는 가상 테이블 생성
+# 1. employee 별 속한 부서 수 구하는 서브쿼리 
 # 2. 부서 수 = 1 또는 primary_flag = 'Y'인 emp_id, dep_id 추출
-WITH Department AS (
+
+SELECT employee_id
+    , department_id
+FROM Employee
+WHERE primary_flag = 'Y' OR
+    employee_id IN (
     SELECT employee_id
-        , COUNT(DISTINCT department_id) as dep_cnt
     FROM Employee
     GROUP BY employee_id
-)
-
-
-SELECT emp.employee_id
-    , emp.department_id
-FROM Employee emp
-    LEFT JOIN Department dep
-        ON emp.employee_id = dep.employee_id
-WHERE dep_cnt = 1 OR primary_flag = 'Y'
+    HAVING COUNT(DISTINCT department_id) = 1
+    )
