75cd2dbc7ba2da766b81d8036bea76070d25ca46
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-08-17 17:10:30 +0900

    Time: 241 ms (76.20%), Space: 0B (100.00%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,8 +1,5 @@
 # 세 변의 길이를 통해 삼각형을 만들 수 있는지 여부 추출
-# 삼각형 조건 : a + b > c , c >= a,b
-# 1. 최대 값 판별
-# 2. 삼각형 판별 
+# 삼각형 조건 : 두 변의 합 > 나머지 한 변의 합
 SELECT *
-    , IF(GREATEST(x,y,z) >= (SUM(x+y+z) - GREATEST(x,y,z)),'No','Yes') as triangle
+    , CASE WHEN x+y>z and x+z>y and y+z>x then 'Yes' else 'No' end as triangle
 FROM Triangle
-GROUP BY x,y,z
