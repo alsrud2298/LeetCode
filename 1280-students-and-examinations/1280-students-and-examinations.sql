b4b1926247ade4e845706c9b2e514dbbea29eac6
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-09-04 22:56:31 +0900

    Time: 757 ms (90.17%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,9 +1,16 @@
-# 학생 별 시험에 참석한 횟수 세기 / 참석한 적 없다면 0번
-# std_id, sub name 순 정렬
-SELECT *
-FROM Students st
-    CROSS JOIN Subjects su
-    LEFT JOIN Examinations e
-        ON st.student_id = e.student_id AND su.subject_name = e.subject_name
-
+-- 학생 별 시험에 참석한 횟수 추출 
+-- 1. 모든 학생 x 과목 조합 테이블 생성 (Students, Subjects 테이블 조인)
+-- 2. Examinations 테이블 LEFT 조인 -> 학생&과목 별 시험 참석 횟수 구하기
+-- 3. student_id, subject_name 기준 정렬
+SELECT Sub.*,
+    COUNT(e.student_id) as attended_exams
+FROM(
+    SELECT *
+    FROM Students
+        CROSS JOIN Subjects
+) as Sub
+    LEFT JOIN Examinations as e
+    ON Sub.student_id = e.student_id AND Sub.subject_name = e.subject_name
+GROUP BY Sub.student_id, Sub.subject_name
+ORDER BY student_id, subject_name
 
