36530dae0bcf5ce5533ee0cc4ed49addf701dbab
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-09-09 17:07:26 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,8 +1,9 @@
-# 대회에 참여한 유저 비율
-# 조건 : ROUND(,2), percentage desc & contest_id asc
+# (복습) 각 contest에 참여한 유저들의 비율 추출
+# 1. 전체 유저 수 구하기
+# 2. 콘테스트 당 참여 유저 수 / 전체 유저 수 
 
-SELECT contest_id
-    , ROUND(COUNT(DISTINCT user_Id) * 100 / (SELECT COUNT(user_id) FROM Users),2) as percentage
-FROM Register
+SELECT contest_id,
+    ROUND(COUNT(user_id) / (SELECT COUNT(DISTINCT user_id) FROM Users),2) percentage
+FROM Users
 GROUP BY contest_id
 ORDER BY percentage DESC, contest_id
