c0b9c69f77d00381a9fea1a56b5740498467dbfe
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-09-11 22:16:28 +0900

    Time: 544 ms (79.21%), Space: 0B (100.00%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,11 +1,10 @@
-# each month, country 별 거래 수,총 거래액, 승인된 거래 수, 승인된 총 거래액 구하기
+-- (복습) 각 월, 나라 별 거래 수, 총 수량, 승인된 거래 수, 승인된 총 수량 추출
 
-SELECT LEFT(trans_date,7) as month
-    , country as country
-    , COUNT(id) as trans_count
-    , SUM(state='approved') as approved_count
-    , SUM(amount) as trans_total_amount
-    , SUM((state='approved')*amount
-    ) as approved_total_amount
+SELECT LEFT(trans_date,7) as month,
+    country,
+    COUNT(*) as trans_count ,
+    SUM(IF(state='approved',1,0)) as approved_count,
+    SUM(amount) as trans_total_amount,
+    SUM(IF(state='approved',amount,0)) as approved_total_amount
 FROM Transactions
-GROUP BY month, country
+GROUP BY LEFT(trans_date,7), country
