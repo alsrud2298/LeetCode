b911a3fd16e812b6359d57af852196c048027ee6
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-09-13 15:28:10 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,12 +1,11 @@
-# 첫날 로그인 & 다음 날 또 로그인한 플레이어들의 비율 계산
+-- (복습) 첫 로그인 & 다음날 또 로그인 한 플레이어의 비율 추출
+-- 1. player 별 첫 로그인 일 구하기 MIN(event_date)
+-- 2. 첫 로그인 + 1일째 로그인 기록이 있는지 체크
 
-SELECT
-  ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
-FROM
-  Activity
-WHERE
-  (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
-  IN (
-    SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id
-  )
-
+SELECT COUNT(DISTINCT player_id)/(SELECT COUNT(DISTINCT player_id) FROM Activity) as fraction
+FROM Activity
+WHERE (player_id, event_date) IN (
+    SELECT player_id,
+    DATE_ADD(MIN(event_date),INTERVAL 1 DAY)
+    FROM Activity
+    GROUP BY player_id)
