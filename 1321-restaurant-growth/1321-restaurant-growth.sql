d1e0d2ca24b18d17c901d0d4755649798d37c631
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-09-23 23:37:11 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,10 @@
+
+SELECT DISTINCT visited_on,
+    SUM(amount) OVER w as amount,
+    ROUND((SUM(amount) OVER w)/7,2) as average_amount
+FROM Customer
+    WINDOW w AS (
+        ORDER BY visited_on
+        RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT NOW
+    )
+LIMIT 6,999
