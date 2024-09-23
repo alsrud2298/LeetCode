50c34867d2d0d686434922fd003887237f773efa
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-09-24 00:17:43 +0900

    Time: 308 ms (85.26%), Space: 0B (100.00%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,10 +1,11 @@
+-- WINDOW 절 FRAME 절로 Rolling 평균 계산
 
 SELECT DISTINCT visited_on,
-    SUM(amount) OVER w as amount,
-    ROUND((SUM(amount) OVER w)/7,2) as average_amount
+    SUM(amount) OVER w as amount, -- 합계
+    ROUND((SUM(amount) OVER w)/7,2) as average_amount -- 매출 평균
 FROM Customer
     WINDOW w AS (
         ORDER BY visited_on
-        RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW
+        RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW -- 현재 행 + 6일 이전 행에 대해서 
     )
 LIMIT 6,999
