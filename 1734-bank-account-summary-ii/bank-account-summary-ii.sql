60aba10b9eb90157c206936cbaa84b75a39f14f3
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2024-12-28 20:35:42 +0900

    Time: 1815 ms (6.70%) | Memory: 0B (100.00%) - LeetSync

--- 1734-bank-account-summary-ii/bank-account-summary-ii.sql
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -1,12 +1,9 @@
 -- 잔고가 10000 초과인 고객의 이름과 잔고 출력하기
+
 SELECT name
-    , balance
-FROM(
-    SELECT t.account
-        , name
-        , SUM(amount) as balance
-    FROM Transactions t
-        JOIN Users u ON t.account = u.account 
-    GROUP BY 1
-    HAVING SUM(amount) > 10000
-) sub
+    , SUM(amount) as balance
+FROM Transactions t
+    JOIN Users u ON t.account = u.account 
+GROUP BY t.account
+HAVING SUM(amount) > 10000
+
