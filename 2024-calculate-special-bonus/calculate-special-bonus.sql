4279a966a10d89dae89446a9a3477812c735ce5d
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2025-01-09 16:06:22 +0900

    Time: 912 ms (37.05%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -0,0 +1,9 @@
+# id가 홀수이고 이름이 M으로 시작하지 않는 직원 -> 월급의 100%
+# 아니라면, 0원
+
+SELECT employee_id
+    , CASE WHEN employee_id IN (SELECT employee_id FROM Employees WHERE MOD(employee_id,2) = 1
+                                AND LEFT(name,1) != 'M') THEN salary
+        ELSE 0 END bonus
+FROM Employees
+ORDER BY 1
