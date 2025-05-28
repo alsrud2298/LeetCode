1887f2cc7248b03b3727176c5df45b173b63ae0a
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2025-05-28 19:39:59 +0900

    Time: 1171 ms (5.05%), Space: 0B (100%) - LeetHub

--- 3767-find-students-who-improved/3767-find-students-who-improved.sql
+++ 3767-find-students-who-improved/3767-find-students-who-improved.sql
@@ -2,50 +2,40 @@
 # 조건 1 : 최소 2개의 다른 날에 같은 과목 시험을 친 경우
 # 조건 2 : 그 과목의 가장 최근 점수가 처음 점수보다 높을 것
 
-WITH more_than_twice AS ( -- 1. 학생, 과목 별 exam_date가 2개 이상인 경우만 추출
-    SELECT
-        student_id,
-        subject,
-        score,
-        exam_date
-    FROM(
-        SELECT
-            student_id,
-            subject,
-            score,
-            exam_date,
-            COUNT(exam_date) OVER (PARTITION BY student_id, subject) AS exam_cnt
-        FROM Scores
-    ) AS t
-    WHERE exam_cnt >= 2
-), exam_date AS ( -- 2. 학생, 과목 별 첫 시험 날짜와 마지막 시험 날짜 구하기
+WITH first_and_last_exam AS ( -- 1. 조건 1 만족하는 데이터만 추출
     SELECT
         student_id,
         subject,
         MIN(exam_date) AS first_exam,
         MAX(exam_date) AS last_exam
-    FROM more_than_twice
+    FROM Scores
     GROUP BY
         student_id,
         subject
-), first_exam_data AS ( -- 3. 첫 시험 성적 구하기
+    HAVING MIN(exam_date) != MAX(exam_date) -- 다른 날짜에 친 시험만
+), first_exam_data AS ( -- 2. 첫 시험 성적 구하기
     SELECT
-        m.student_id,
-        m.subject,
-        m.score AS first_score
-    FROM more_than_twice m
-    JOIN exam_date e
-    ON m.student_id = e.student_id AND m.subject = e.subject AND m.exam_date = e.first_exam
-), last_exam_data AS ( -- 4. 마지막 시험 성적 구하기
+        s.student_id,
+        s.subject,
+        s.score AS first_score
+    FROM Scores s
+    JOIN first_and_last_exam f
+    ON s.student_id = f.student_id
+    AND s.subject = f.subject
+    AND s.exam_date = f.first_exam
+), last_exam_data AS ( -- 3. 마지막 시험 성적 구하기
     SELECT
-        m.student_id,
-        m.subject,
-        m.score AS latest_score
-    FROM more_than_twice m
-    JOIN exam_date e
-    ON m.student_id = e.student_id AND m.subject = e.subject AND m.exam_date = e.last_exam
+        s.student_id,
+        s.subject,
+        s.score AS latest_score
+    FROM Scores s
+    JOIN first_and_last_exam f
+    ON s.student_id = f.student_id
+    AND s.subject = f.subject
+    AND s.exam_date = f.last_exam
 )
--- 5. 첫 시험  성적 < 마지막 시험 성적인 데이터만 추출 ! 
+
+-- 4. 첫 시험  성적 < 마지막 시험 성적인 데이터만 추출 ! 
 SELECT
     f.student_id,
     f.subject,
@@ -54,3 +44,6 @@ SELECT
 FROM first_exam_data f
 JOIN last_exam_data l
 ON f.student_id = l.student_id AND f.subject = l.subject AND f.first_score < l.latest_score
+ORDER BY
+    student_id,
+    subject
