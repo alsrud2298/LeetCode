4459a8e5d06447493288b15e8e25f85fe69bfd00
Mingyeong Ma <76714310+alsrud2298@users.noreply.github.com>
2025-05-28 19:40:16 +0900

    Time: 353 ms (14.36%), Space: 0B (100%) - LeetHub

--- 3767-find-students-who-improved/3767-find-students-who-improved.sql
+++ 3767-find-students-who-improved/3767-find-students-who-improved.sql
@@ -1,49 +1,25 @@
-# 성장한 학생 찾기
+# 성장한 학생 찾기 window function.ver
 # 조건 1 : 최소 2개의 다른 날에 같은 과목 시험을 친 경우
 # 조건 2 : 그 과목의 가장 최근 점수가 처음 점수보다 높을 것
-
-WITH first_and_last_exam AS ( -- 1. 조건 1 만족하는 데이터만 추출
+-- RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW 가 기본 값.
+WITH temp AS (
     SELECT
+        DISTINCT
         student_id,
         subject,
-        MIN(exam_date) AS first_exam,
-        MAX(exam_date) AS last_exam
+        FIRST_VALUE(score) OVER(PARTITION BY student_id, subject ORDER BY exam_date) first_score,
+        FIRST_VALUE(score) OVER(PARTITION BY student_id, subject ORDER BY exam_date DESC) AS latest_score,
+        COUNT(student_id) OVER(PARTITION BY student_id, subject) AS diff_dates
     FROM Scores
-    GROUP BY
-        student_id,
-        subject
-    HAVING MIN(exam_date) != MAX(exam_date) -- 다른 날짜에 친 시험만
-), first_exam_data AS ( -- 2. 첫 시험 성적 구하기
-    SELECT
-        s.student_id,
-        s.subject,
-        s.score AS first_score
-    FROM Scores s
-    JOIN first_and_last_exam f
-    ON s.student_id = f.student_id
-    AND s.subject = f.subject
-    AND s.exam_date = f.first_exam
-), last_exam_data AS ( -- 3. 마지막 시험 성적 구하기
-    SELECT
-        s.student_id,
-        s.subject,
-        s.score AS latest_score
-    FROM Scores s
-    JOIN first_and_last_exam f
-    ON s.student_id = f.student_id
-    AND s.subject = f.subject
-    AND s.exam_date = f.last_exam
 )
 
--- 4. 첫 시험  성적 < 마지막 시험 성적인 데이터만 추출 ! 
 SELECT
-    f.student_id,
-    f.subject,
-    f.first_score,
-    l.latest_score
-FROM first_exam_data f
-JOIN last_exam_data l
-ON f.student_id = l.student_id AND f.subject = l.subject AND f.first_score < l.latest_score
+    student_id,
+    subject,
+    first_score,
+    latest_score
+FROM temp
+WHERE first_score < latest_score AND diff_dates > 1
 ORDER BY
     student_id,
     subject
